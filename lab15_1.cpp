4575c43bd676f3f223fda47f3d80525abcdd0439
Nakinate Buatip <nakinate1710@gmail.com>
2022-01-11 16:17:07 +0700

    comple

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,9 +3,26 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for(int k = 1; k<N ; k++)
+	{
+		for(int i=k; i>0;i--)
+		{
+			if(d[i]>d[i-1]){
+				double temp = d[i];
+				d[i] =d[i-1];
+				d[i-1]=temp;
+			}
+		}
+		
+		cout << "Pass " << k << ":" ;
+		
+		for(int y=0; y<N ; y++)
+		{
+			cout << d[y] << " ";
+		}
+		cout << "\n";
+	}
 }
-
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
 	cout << "Input Array:";
@@ -17,3 +34,5 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+ 
+
